c07e747d23ff74d886b41baa7027777aa1d6f32b
abdelmoniem ahmed <3bdelmoniemahmed@gmail.com>
2023-01-12 20:10:29 +0200

    finished

--- js/BDA.js
+++ js/BDA.js
@@ -1,20 +1,13 @@
+$(window).scroll(function () {
+  "use strict";
+  /* Check the location of each desired element */
+  $(".statistics").each(function (i) {
+    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-$(window).scroll( function(){
-
-    "use strict";
-    /* Check the location of each desired element */
-    $('.statistics').each( function(i){
-
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-        /* If the object is completely visible in the window, fade it in */
-        if( bottom_of_window > bottom_of_object ){
-
-            $(".count").countTo();
-
-        }
-
-    }); 
-
+    /* If the object is completely visible in the window, fade it in */
+    if (bottom_of_window > bottom_of_object) {
+      $(".count").countTo();
+    }
+  });
 });
